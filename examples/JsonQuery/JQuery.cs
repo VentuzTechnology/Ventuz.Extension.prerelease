ac58d68ec243c4aaddefc9b086596dcbadc0268a
Eric Gielians <gielians@realtime-department.de>
2022-10-07 14:23:50 +0200

    Added JsonQuery Example
    With the "JsonQuery" content node you can use the "SelectTokens" funktion directly on a Json String which allows for simple or fairly advanced Queries on JObjects or JArrays. The Output is also a Json string that can be used with a Json parser.

---
+++ examples/JsonQuery/JQuery.cs
@@ -0,0 +1,74 @@
+﻿using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ventuz.Extension;
+using Ventuz.Kernel;
+
+namespace RDVX
+{
+    [VxToolBox("Json Query", "Data", "JQuery", "Selects Tokens from Json Strings and outputs them as arry", false)]
+    [VxHelpUrl("https://www.realtime-department.de", "Realtime Department Home Page")]
+    [VxHelpUrl("https://www.newtonsoft.com/json/help/html/SelectToken.htm", "This Node uses \"Select Tokens\" as described here")]
+    [VxIcon("NodeIcons.Data.Json")]
+    [VxDescriptionAttribute("One or more queries as string or string array","Queries")]
+    [VxDescriptionAttribute("The Json String to be parsed", "JsonString")]
+    [VxDescriptionAttribute("Content of the parsed Tokens. Use Json Parser node to select the values", "JsonTokens")]
+    [VxCategory(1, "Json", false, "JsonString", "Queries","JsonTokens")]
+    public class JsonQuery : VxContentNode
+    {
+        public JContainer Validate_JsonContainer(string JsonString)
+        {
+            JContainer parsedcontainer = null;
+            if (JsonString.Trim()[0] == '[')
+            {
+                try
+                {
+                    parsedcontainer = JArray.Parse(JsonString);
+                }
+                catch (Exception ex)
+                {
+                    VLog.Error("JsonQuery", $"Could not parse Array: {ex}", VPopup.Never);
+                }
+            }
+            if (JsonString.Trim()[0] == '{')
+            {
+                try
+                {
+                    parsedcontainer = JObject.Parse(JsonString);
+                }
+                catch (Exception ex)
+                {
+                    VLog.Error("JsonQuery", $"Could not parse Object: {ex}", VPopup.Never);
+                }
+            }
+            return parsedcontainer;
+        }
+        public string ValidateJsonTokens(JContainer _JsonContainer, string[] Queries)
+        {
+            string outputJson = null;
+            if (_JsonContainer != null && Queries != null && Queries.Any())
+            {
+                var outputlist = new Dictionary<string, IEnumerable<JToken>>();
+                
+                foreach (var propertyname in Queries)
+                {
+                    var tokens = _JsonContainer.SelectTokens(propertyname, false);
+                    if (tokens != null && tokens.Any())
+                    {
+                        outputlist[propertyname] = _JsonContainer.SelectTokens(propertyname, false);
+                    }
+                }
+                if (outputlist.Any())
+                {
+                    outputJson = JsonConvert.SerializeObject(outputlist);
+                }
+            }
+            return outputJson;
+        }
+    }
+}
---
+++ examples/JsonQuery/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Allgemeine Informationen über eine Assembly werden über die folgenden
+// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+// die einer Assembly zugeordnet sind.
+[assembly: AssemblyTitle("JsonQuery")]
+[assembly: AssemblyDescription("Ventuz Extention JsonQuery")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Realtime Department GmbH")]
+[assembly: AssemblyProduct("Realtime Departmetn Ventuz Extentions")]
+[assembly: AssemblyCopyright("Copyright ©  Realtime Department 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Durch Festlegen von ComVisible auf FALSE werden die Typen in dieser Assembly
+// für COM-Komponenten unsichtbar.  Wenn Sie auf einen Typ in dieser Assembly von
+// COM aus zugreifen müssen, sollten Sie das ComVisible-Attribut für diesen Typ auf "True" festlegen.
+[assembly: ComVisible(false)]
+
+// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+[assembly: Guid("192747f5-70b4-4c73-aabb-a9028e4b432e")]
+
+// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+//
+//      Hauptversion
+//      Nebenversion
+//      Buildnummer
+//      Revision
+//
+// Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
+// indem Sie "*" wie unten gezeigt eingeben:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
