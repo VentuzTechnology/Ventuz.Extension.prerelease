3ba4c5d93b90f4ddd199db3259fc7d34356b6d4f
Sebastian M. Rouaiha <sr@glare-productions.com>
2022-10-11 09:01:55 +0200

    added StringCompress content node for compressing and decompressing strings

---
+++ examples/StringCompress/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("StringCompress")]
+[assembly: AssemblyDescription("Ventuz Extension for compressing strings")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Glare Productions GmbH")]
+[assembly: AssemblyProduct("StringCompress")]
+[assembly: AssemblyCopyright("Copyright © 2022 Glare Productions GmbH")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8e9a2616-9d8d-4a81-8236-7da69da758f8")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.9.0.0")]
+[assembly: AssemblyFileVersion("0.9.0.0")]
---
+++ examples/StringCompress/StringCompress.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ventuz.Extension;
+using Ventuz.Kernel;
+using System.IO;
+using System.IO.Compression;
+
+namespace Glare.VX
+{
+	/// <summary>
+	/// Compression Method
+	/// </summary>
+	public enum CompressionMethodEnum
+	{
+		/// <summary>
+		/// No compression, used to disable compression.
+		/// </summary>
+		None,
+
+		/// <summary>
+		/// GZip compression
+		/// </summary>
+		GZip,
+
+		/// <summary>
+		/// Deflate algorithm
+		/// </summary>
+		Deflate
+	}
+
+	[VxToolBox("String Compress VX", "Text", "Compress", "Compress a given string. The result is then base64-encoded.", false)]
+	[VxIcon("NodeIcons.Logic.Expressions")]
+
+	[VxDescriptionAttribute("The compression method to be used", "Method")]
+	[VxCategory(0, "Control", false, "Method")]
+	[VxDefaultValue(typeof(CompressionMethodEnum), "1", "Method")]
+
+	[VxDescriptionAttribute("The text that will be compressed", "Text")]
+	[VxCategory(1, "Input", false, "Text")]
+	
+	[VxCategory(2, "Output", false, "Compressed")]
+	[VxDescriptionAttribute("The compressed and then base64-encoded text", "Compressed")]
+	public class StringCompress : VxContentNode
+	{
+		/// <summary>
+		/// Compress a given string and then base64 encode it.
+		/// </summary>
+		/// <param name="text">The text to be compressed.</param>
+		/// <param name="type">The compression type to be used.</param>
+		/// <returns>A base64-encoded compressed version of <paramref name="text"/></returns>
+		protected string CompressString(string text, CompressionMethodEnum type)
+		{
+			// avoid compressing thin air
+			if (string.IsNullOrEmpty(text)) return string.Empty;
+
+			// not enabled
+			if(type == CompressionMethodEnum.None) return text;
+
+			try
+			{
+				byte[] buffer = Encoding.UTF8.GetBytes(text);
+				var memoryStream = new MemoryStream();
+
+				switch (type)
+				{
+					case CompressionMethodEnum.GZip:
+						using (var gZipStream = new GZipStream(memoryStream, CompressionMode.Compress, true))
+						{
+							gZipStream.Write(buffer, 0, buffer.Length);
+						}
+						break;
+					case CompressionMethodEnum.Deflate:
+						using (var deflateStream = new DeflateStream(memoryStream, CompressionMode.Compress, true))
+						{
+							deflateStream.Write(buffer, 0, buffer.Length);
+						}
+						break;
+				}
+				
+
+				memoryStream.Position = 0;
+
+				var compressedData = new byte[memoryStream.Length];
+				memoryStream.Read(compressedData, 0, compressedData.Length);
+
+				var compressedBuffer = new byte[compressedData.Length + 4];
+				Buffer.BlockCopy(compressedData, 0, compressedBuffer, 4, compressedData.Length);
+				Buffer.BlockCopy(BitConverter.GetBytes(buffer.Length), 0, compressedBuffer, 0, 4);
+				
+				return Convert.ToBase64String(compressedBuffer);
+			}
+			catch (Exception ex)
+			{
+				VLog.Error("Glare.VX.StringCompress", $"Could not compress string: {ex.Message}", VPopup.Never);
+				return string.Empty;
+			}
+		}
+
+		protected string ValidateCompressed(string Text, CompressionMethodEnum Method) => CompressString(Text, Method);
+    }
+
+	
+	[VxToolBox("String Decompress VX", "Text", "Decompress", "Base64-decode a given string and then decompress it.", false)]
+	[VxIcon("NodeIcons.Logic.Expressions")]
+
+	[VxDescriptionAttribute("The decompression method to be used", "Method")]
+	[VxCategory(0, "Control", false, "Method")]
+	[VxDefaultValue(typeof(CompressionMethodEnum), "1", "Method")]
+
+	[VxDescriptionAttribute("The base64-encoded data that will be decompressed", "Compressed")]
+	[VxCategory(1, "Input", false, "Compressed")]
+
+	[VxCategory(2, "Output", false, "Text")]
+	[VxDescriptionAttribute("The compressed and then base64-encoded text", "Text")]
+	public class StringDecompress : VxContentNode
+	{
+		/// <summary>
+		/// Base64-decode a gfiven string and then gzip-decompress it.
+		/// </summary>
+		/// <param name="compressed">The compressed and base64-encoded data.</param>
+		/// <returns>The decompressed text</returns>
+		protected string DecompressString(string compressed, CompressionMethodEnum type)
+		{
+			// avoid compressing thin air
+			if (string.IsNullOrEmpty(compressed)) return string.Empty;
+
+			// not enabled
+			if (type == CompressionMethodEnum.None) return compressed;
+
+			try
+			{
+				byte[] rawBuffer = Convert.FromBase64String(compressed);
+				using (var memoryStream = new MemoryStream())
+				{
+					int dataLength = BitConverter.ToInt32(rawBuffer, 0);
+					memoryStream.Write(rawBuffer, 4, rawBuffer.Length - 4);
+
+					var buffer = new byte[dataLength];
+
+					memoryStream.Position = 0;
+					switch (type)
+					{
+						
+						case CompressionMethodEnum.GZip:
+							using (var gZipStream = new GZipStream(memoryStream, CompressionMode.Decompress))
+							{
+								gZipStream.Read(buffer, 0, buffer.Length);
+							}
+							break;
+						
+						case CompressionMethodEnum.Deflate:
+							using (var deflateStream = new DeflateStream(memoryStream, CompressionMode.Decompress))
+							{
+								deflateStream.Read(buffer, 0, buffer.Length);
+							}
+							break;
+						
+					}
+					
+
+					return Encoding.UTF8.GetString(buffer);
+				}
+			}
+			catch (Exception ex)
+			{
+				VLog.Error("Glare.VX.StringDecompress", $"Could not decompress string: {ex.Message}", VPopup.Never);
+				return string.Empty;
+			}
+		}
+
+		protected string ValidateText(string Compressed, CompressionMethodEnum Method) => DecompressString(Compressed, Method);
+	}
+}
